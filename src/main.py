a1e9c6a676472b61ecc41001850348e3976cc73b
Roman Bogdanov <lackroy5111@gmail.com>
2023-03-22 01:20:14 +0700

    Переделал функцию main. Переделал функцию, которая печатает данные, теперь она ретернит данные в подготовленном для вывода на экран виде, а main их просто печатает. Написал для не тесты.

--- src/main.py
+++ src/main.py
@@ -1,41 +1,16 @@
-from datetime import datetime
 from os.path import join
 from utils.utils import load_data
 from utils.last_five_transactions import find_last_transactions
-from utils.last_five_transactions import split_card_number
+from utils.last_five_transactions import output_data
 
 
 def main():
-    """
-    Просто выводит все в консоль
-    """
+    """Просто все печатаем"""
+    for data in output_data(find_last_transactions(load_data(join('..', 'data', 'operations.json')))):
 
-    # Получили 5 последних транзакций
-    last_transactions = find_last_transactions(load_data(join('..', 'data', 'operations.json')))
-
-    # В одной итерации цикла распечатываются данные одной транзакции
-    for i in range(len(last_transactions)):
-        date = datetime.strptime(last_transactions[i]['date'].split('.')[0], "%Y-%m-%dT%H:%M:%S")
-
-        # Вывести дату в нужном формате и название операции
-        print(f'{date.day}.{date.month}.{date.year} {last_transactions[i]["description"]}')
-
-        # Если операция - не "открытие вклада", выводится номер карты отправителя
-        if last_transactions[i]["description"] != "Открытие вклада":
-            print(
-                f'{last_transactions[i]["from"].split(" ")[0]} '
-                f'{split_card_number(last_transactions[i]["from"].split(" ")[-1])} -> '
-                f'Счет **{last_transactions[i]["to"].split(" ")[-1][-4:]}'
-            )
-        # Если операция "открытие вклада" выводится только номер карты получателя
-        else:
-            print(f'Счет **{last_transactions[i]["to"].split(" ")[-1][-4:]}')
-
-        # Выводится сумма и валюта операции
-        print(
-            last_transactions[i]["operationAmount"]["amount"] + ' ' +
-            last_transactions[i]["operationAmount"]["currency"]["name"]
-        )
+        print(data['date'])
+        print(data['from_to'])
+        print(data['amount_and_name'])
         print()
 
 
